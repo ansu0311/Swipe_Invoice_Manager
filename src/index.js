a1fb13abb0d1938272ccd6058be36b20131e7718
Ansuman Nayak <nsatyam95@gmail.com>
2023-12-28 17:03:47 +0530

    Added additional information for better code understanding

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@ const store = configureStore({
   },
 });
 
+// Added Redux store usng provider function
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
